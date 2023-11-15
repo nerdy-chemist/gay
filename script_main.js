aa633ab574687077ddb94511b8f677bb03dc44f0
nerdy.chemist <118447268+nerdy-chemist@users.noreply.github.com>
2023-11-15 21:13:47 +0100

    Update script_main.js

--- script_main.js
+++ script_main.js
@@ -1,6 +1,5 @@
-const text = "Herzlichen Wilkomen \nzu mein Präsentation über \nNürnberger Prozese";
+const text = "nerdy.chemist";
 let typingIndex = 0;
-const colors = ["red","red","red"]
 function typing() {
     if (typingIndex < text.length) {
         document.getElementById("willkommen_text").innerHTML += text[typingIndex];
@@ -8,20 +7,3 @@ function typing() {
         setTimeout(typing, 50);
     }
 }
-
-let numb = 1; // Initialize numb to 1 for the background image
-
-function slide_main_page() {
-    numb++;
-    if (numb > 3) {
-        numb = 1; // Reset numb when it reaches 4
-    }
-    const imageUrl = `url('../nurnburger_prozes/${numb}.jpg')`;
-    document.body.style.backgroundImage = imageUrl;
-    document.getElementById("willkommen_text").style.color = colors[numb-1];
-    setTimeout(slide_main_page, 3000);
-};
-
-function openAnotherFile() {
-    window.location.href = '../nurnburger_prozes/p1.html';
-};
